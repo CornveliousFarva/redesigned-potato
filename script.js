0238aff99102a83c421dd504187414738445ace4
Greg Schuman <gcschuman@hotmail.com>
2021-01-18 20:37:23 -0800

    Added a container in the html to make it mobile-friendly and fixed a bug in the script.

--- script.js
+++ script.js
@@ -2,51 +2,64 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=8c0e0b1b99e51f1340602651531679a6&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-const main = document.getElementById('main')
 
 // Get initial movies
 getMovies(API_URL)
 
-async function getMovies(url){
+async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
+
+    showMovies(data.results)
 }
 
-function showMovies(movies){
+function showMovies(movies) {
     main.innerHTML = ''
+
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
-        movieEl.innerHTML = ` <div class="movie">
-        <img src="" alt="">
-        <div class="movie-info">
-            <h3>Movie Title</h3>
-            <span class="green"></span>
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
         </div>
-        <div class="overview">
-            <h3>Overview</h3>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quo sint officiis ducimus tempore porro! Quos, est! Est in officiis beatae molestias ut dolorem quam?
-        </div>
-    </div>`
-    });
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const searchTerm = search
+    const searchTerm = search.value
 
-    if(searchTerm && searchTerm !== ''){
+    if(searchTerm && searchTerm !== '') {
         getMovies(SEARCH_URL + searchTerm)
 
         search.value = ''
-    } else{
+    } else {
         window.location.reload()
     }
 })
-
