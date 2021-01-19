6f6c12f0694812279e3d54407a241162909a78c9
Greg Schuman <gcschuman@hotmail.com>
2021-01-18 19:54:44 -0800

    Added api declarations.

---
+++ script.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8c0e0b1b99e51f1340602651531679a6&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=8c0e0b1b99e51f1340602651531679a6&query="'
